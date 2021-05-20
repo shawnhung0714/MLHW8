3a30b66535dab8ed3e9599c6c085c17ff87e4961
Shawn Hung <shawnhung0714@gmail.com>
2021-05-21 02:59:38 +0800

    Clean config

--- config.py
+++ config.py
@@ -1,13 +1,8 @@
 from argparse import Namespace
-from logging import fatal
 
 config = Namespace(
     root_dir="data-bin",
-    max_question_len=40,
-    max_paragraph_len=250,
-    doc_stride=75,
     batch_size=10000,
-    logging_step=100,
     save_path="models",
     optim_hparas={"lr": 1e-3,},
     n_epochs=50,
